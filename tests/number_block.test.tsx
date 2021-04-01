13072249276f241747f983a82e955de8db3eeb58
Bruno Jan <brunojan@boyu66.net>
2021-04-01 10:53:58 +0800

    refactor

--- tests/number_block.test.tsx
+++ tests/number_block.test.tsx
@@ -21,7 +21,11 @@ const TestingComponent = (props: { [index: string]: number } = initState) => {
 }
 
 
-
+const resetButton = /reset/i;
+const number3 = /3$/i;
+const number4 = /4$/i;
+const number1 = /1$/i;
+const number2 = /2$/i;
 describe('number block component test', () => {
     afterEach(cleanup)
     const getRender = () => render(<TestingComponent/>);
@@ -35,11 +39,11 @@ describe('number block component test', () => {
     const getNumberAreaSelected = () => {
         const component = render(<TestingComponent/>);
 
-        fireEvent.click(component.getAllByText(/1$/i)[0]);
-        fireEvent.click(component.getAllByText(/2$/i)[0]);
+        fireEvent.click(component.getAllByText(number1)[0]);
+        fireEvent.click(component.getAllByText(number2)[0]);
 
-        fireEvent.click(component.getAllByText(/3$/i)[1]);
-        fireEvent.click(component.getAllByText(/4$/i)[1]);
+        fireEvent.click(component.getAllByText(number3)[1]);
+        fireEvent.click(component.getAllByText(number4)[1]);
 
         fireEvent.click(component.getAllByText(/5$/i)[2]);
         fireEvent.click(component.getAllByText(/6$/i)[2]);
@@ -49,115 +53,115 @@ describe('number block component test', () => {
 
         return component;
     }
+
+    const triggerNumberClick = (numberButton: RegExp, area: number) => {
+        const { getAllByText }: any = getRender();
+        fireEvent.click(getAllByText(numberButton)[area]);
+        return getAllByText;
+    };
+
     it('component should be render number 10 button and 4 areas', () => {
         const component = getRender();
         expect(component.container.querySelectorAll("button").length).toBeGreaterThanOrEqual(40)
     });
-
+    
     it('when number 1 button click should be call context updated', (): void => {
-        const { getAllByText }: any = getRender();
-        fireEvent.click(getAllByText(/1$/i)[0]);
+        const getAllByText = triggerNumberClick(number1, 0);
 
-        expect(getAllByText(/1$/i)[0].className).toEqual("selected");
+        expect(getAllByText(number1)[0].className).toEqual("selected");
     });
 
     it('when number 2 button click should be call context updated', (): void => {
-        const { getAllByText }: any = getRender();
-        fireEvent.click(getAllByText(/2$/i)[0]);
+        const getAllByText = triggerNumberClick(number2, 0);
 
-        expect(getAllByText(/2$/i)[0].className).toEqual("selected");
+        expect(getAllByText(number2)[0].className).toEqual("selected");
     });
 
     it('when line 1 number "1" is sedected line 2 number "1" should be disabled', (): void => {
-        const { getAllByText }: any = getRender();
-        fireEvent.click(getAllByText(/1$/i)[0]);
+        const getAllByText = triggerNumberClick(number1, 0);
 
-        expect(getAllByText(/1$/i)[0].className).toEqual("selected");
-        expect(getAllByText(/1$/i)[0].disabled).toBeFalsy();
-        expect(getAllByText(/1$/i)[1].disabled).toBeTruthy();
+        expect(getAllByText(number1)[0].className).toEqual("selected");
+        expect(getAllByText(number1)[0].disabled).toBeFalsy();
+        expect(getAllByText(number1)[1].disabled).toBeTruthy();
     });
 
     it('when line 1 number "2" is sedected line 2 number "2" should be disabled', (): void => {
-        const { getAllByText }: any = getRender();
-        fireEvent.click(getAllByText(/2$/i)[0]);
+        const getAllByText = triggerNumberClick(number2, 0);
 
-        expect(getAllByText(/2$/i)[0].className).toEqual("selected");
-        expect(getAllByText(/2$/i)[0].disabled).toBeFalsy();
-        expect(getAllByText(/2$/i)[1].disabled).toBeTruthy();
+        expect(getAllByText(number2)[0].className).toEqual("selected");
+        expect(getAllByText(number2)[0].disabled).toBeFalsy();
+        expect(getAllByText(number2)[1].disabled).toBeTruthy();
     });
 
     it('when line 1 number "3" is sedected line 2 number "3" should be disabled', (): void => {
-        const { getAllByText }: any = getRender();
-        fireEvent.click(getAllByText(/3$/i)[0]);
+        const getAllByText = triggerNumberClick(number3, 0);
 
-        expect(getAllByText(/3$/i)[0].className).toEqual("selected");
-        expect(getAllByText(/3$/i)[0].disabled).toBeFalsy();
-        expect(getAllByText(/3$/i)[1].disabled).toBeTruthy();
+        expect(getAllByText(number3)[0].className).toEqual("selected");
+        expect(getAllByText(number3)[0].disabled).toBeFalsy();
+        expect(getAllByText(number3)[1].disabled).toBeTruthy();
     });
 
     it('when line 1 number "1" is sedected other line number "1" should be disabled', (): void => {
-        const { getAllByText }: any = getRender();
-        fireEvent.click(getAllByText(/1$/i)[0]);
+        const getAllByText = triggerNumberClick(number1, 0);
 
-        expect(getAllByText(/1$/i)[0].className).toEqual("selected");
-        expect(getAllByText(/1$/i)[0].disabled).toBeFalsy();
-        expect(getAllByText(/1$/i)[1].disabled).toBeTruthy();
-        expect(getAllByText(/1$/i)[2].disabled).toBeTruthy();
+        expect(getAllByText(number1)[0].className).toEqual("selected");
+        expect(getAllByText(number1)[0].disabled).toBeFalsy();
+        expect(getAllByText(number1)[1].disabled).toBeTruthy();
+        expect(getAllByText(number1)[2].disabled).toBeTruthy();
     });
 
     it('when area 2 number "1" is sedected other area number "1" should be disabled', (): void => {
-        const { getAllByText }: any = getRender();
-        fireEvent.click(getAllByText(/1$/i)[1]);
+        const getAllByText = triggerNumberClick(number1, 1);
 
-        expect(getAllByText(/1$/i)[1].className).toEqual("selected");
-        expect(getAllByText(/1$/i)[1].disabled).toBeFalsy();
-        expect(getAllByText(/1$/i)[0].disabled).toBeTruthy();
-        expect(getAllByText(/1$/i)[2].disabled).toBeTruthy();
+        expect(getAllByText(number1)[1].className).toEqual("selected");
+        expect(getAllByText(number1)[1].disabled).toBeFalsy();
+        expect(getAllByText(number1)[0].disabled).toBeTruthy();
+        expect(getAllByText(number1)[2].disabled).toBeTruthy();
     });
 
     it('when number 1 already selected, click number 1 should be unselect', (): void => {
-        const { getAllByText }: any = getNumberAlreadySelected(/1$/i, 1);
-        fireEvent.click(getAllByText(/1$/i)[0]);
+        const { getAllByText }: any = getNumberAlreadySelected(number1, 1);
+        fireEvent.click(getAllByText(number1)[0]);
 
-        expect(getAllByText(/1$/i)[0].className).toBe("");
-        expect(getAllByText(/1$/i)[0].disabled).toBeFalsy();
-        expect(getAllByText(/1$/i)[1].disabled).toBeFalsy();
-        expect(getAllByText(/1$/i)[2].disabled).toBeFalsy();
+        expect(getAllByText(number1)[0].className).toBe("");
+        expect(getAllByText(number1)[0].disabled).toBeFalsy();
+        expect(getAllByText(number1)[1].disabled).toBeFalsy();
+        expect(getAllByText(number1)[2].disabled).toBeFalsy();
     });
 
     it('when number 2 already selected, click number 2 should be unselect', (): void => {
-        const { getAllByText }: any = getNumberAlreadySelected(/2$/i, 1);
-        fireEvent.click(getAllByText(/2$/i)[0]);
+        const { getAllByText }: any = getNumberAlreadySelected(number2, 1);
+        fireEvent.click(getAllByText(number2)[0]);
 
-        expect(getAllByText(/2$/i)[0].className).toBe("");
-        expect(getAllByText(/2$/i)[0].disabled).toBeFalsy();
-        expect(getAllByText(/2$/i)[1].disabled).toBeFalsy();
-        expect(getAllByText(/2$/i)[2].disabled).toBeFalsy();
+        expect(getAllByText(number2)[0].className).toBe("");
+        expect(getAllByText(number2)[0].disabled).toBeFalsy();
+        expect(getAllByText(number2)[1].disabled).toBeFalsy();
+        expect(getAllByText(number2)[2].disabled).toBeFalsy();
     });
 
     it('when area 2 number 2 already selected, click number 2 should be unselect', (): void => {
-        const { getAllByText }: any = getNumberAlreadySelected(/2$/i, 2);
-        fireEvent.click(getAllByText(/2$/i)[1]);
+        const { getAllByText }: any = getNumberAlreadySelected(number2, 2);
+        fireEvent.click(getAllByText(number2)[1]);
 
-        expect(getAllByText(/1$/i)[1].className).toBe("");
-        expect(getAllByText(/2$/i)[1].disabled).toBeFalsy();
-        expect(getAllByText(/2$/i)[0].disabled).toBeFalsy();
-        expect(getAllByText(/2$/i)[2].disabled).toBeFalsy();
+        expect(getAllByText(number2)[1].className).toBe("");
+        expect(getAllByText(number2)[1].disabled).toBeFalsy();
+        expect(getAllByText(number2)[0].disabled).toBeFalsy();
+        expect(getAllByText(number2)[2].disabled).toBeFalsy();
     });
 
     it('when area 1 reset button click, the area selected number should be clean', (): void => {
         const { getAllByText }: any = getNumberAreaSelected();
-        fireEvent.click(getAllByText(/reset/i)[0]);
+        fireEvent.click(getAllByText(resetButton)[0]);
 
-        expect(getAllByText(/1$/i)[0].className).toBe("");
-        expect(getAllByText(/2$/i)[0].className).toBe("");
+        expect(getAllByText(number1)[0].className).toBe("");
+        expect(getAllByText(number2)[0].className).toBe("");
     });
 
     it('when area 2 reset button click, the area selected number should be clean', (): void => {
         const { getAllByText }: any = getNumberAreaSelected();
-        fireEvent.click(getAllByText(/reset/i)[1]);
+        fireEvent.click(getAllByText(resetButton)[1]);
 
-        expect(getAllByText(/3$/i)[0].className).toBe("");
-        expect(getAllByText(/4$/i)[0].className).toBe("");
+        expect(getAllByText(number3)[0].className).toBe("");
+        expect(getAllByText(number4)[0].className).toBe("");
     });
 })
